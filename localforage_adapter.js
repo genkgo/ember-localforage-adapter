383dbd9c7a2b7c896152417bb4103e45da94a42f
frederik <f.bosch@genkgo.nl>
2015-01-15 14:26:56 +0100

    cache implementation for 0.5.* serie

--- localforage_adapter.js
+++ localforage_adapter.js
@@ -115,28 +115,31 @@
   });
   
   DS.LFCache = Ember.Object.extend ({
-	  
-	  data : [],
-	  
-	  clear : function () {
-		  this.data = [];
-	  },
-	  
-	  get : function (namespace) {
-		  if (this.data[namespace]) {
-			  return Ember.copy(Ember.A(this.data[namespace]), true);
-		  } else {
-			  return null;
-		  }
-	  },
-	  
-	  set : function (namespace, objects) {
-		  this.data[namespace] = Ember.copy(Ember.A(objects), true);
-	  },
-	  
-	  replace : function (data) {
-		  this.data = Ember.copy(Ember.A(data), true);
-	  }
+
+      data : Ember.Map.create(),
+
+      clear : function () {
+          this.data.clear();
+      },
+
+      get : function (namespace) {
+          if (this.data.get(namespace)) {
+              return Ember.copy(Ember.A(this.data.get(namespace)), true);
+          } else {
+              return null;
+          }
+      },
+
+      set : function (namespace, objects) {
+          this.data.set(namespace, Ember.copy(Ember.A(objects), true));
+      },
+
+      replace : function (data) {
+          this.clear();
+          for (var index in data) {
+              this.set(index, data[index]);
+          }
+      }
 	  
   });
 
