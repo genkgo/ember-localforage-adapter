a6070fd186ad68331f3ca7eef26dcc1d0046ae91
Per Lundberg <per.lundberg@ecraft.com>
2015-04-16 12:41:39 +0300

    Updated model also.

--- tests/dummy/app/models/customer.js
+++ tests/dummy/app/models/customer.js
@@ -1,9 +1,7 @@
 import DS from 'ember-data';
 
-var attr = DS.attr;
-var hasMany = DS.hasMany;
-
 export default DS.Model.extend({
   customerNumber: DS.Model(),
-  addresses: hasMany('address')
+  parentCustomer: DS.belongsTo('customer'),
+  addresses: DS.hasMany('address')
 });
