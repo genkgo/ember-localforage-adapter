eccda970ed35626b5d7b0ea4856d957ab992de69
Per Lundberg <per.lundberg@ecraft.com>
2015-04-16 10:16:57 +0300

    Added a negative test also.

--- tests/unit/serializers/customer-test.js
+++ tests/unit/serializers/customer-test.js
@@ -8,7 +8,13 @@ moduleFor('serializer:customer', {
   // needs: ['serializer:foo']
 });
 
-test('it is set up with embedded:always style for the addresses relation', function(assert) {
+test("it is set up with embedded:always style for the 'addresses' relation", function(assert) {
   var serializer = this.subject();
   assert.ok(serializer.hasEmbeddedAlwaysOption('addresses'), 'addresses relation is not set up with embedded:always style');
 });
+
+test("it is not set up with embedded:always style for the 'foo' relation", function(assert) {
+  var serializer = this.subject();
+  assert.ok(!serializer.hasEmbeddedAlwaysOption('foo'), 'foo relation is unexpectedly set up with embedded:always style');
+});
+
