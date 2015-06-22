e12c95e8eb52a28f88d05de2dde32f5932f48831
iggor_kurr <kurinnoyi@gmail.com>
2015-06-22 20:18:49 +0300

    try fix

--- index.js
+++ index.js
@@ -4,8 +4,11 @@
 module.exports = {
   name: 'ember-localforage-adapter',
   included: function included(app) {
-    this._super.included(app);
-
+    this._super.included.apply(this, arguments);
+    if (app.app) {
+      app = app.app;
+    }
+    this.app = app;
     app.import(app.bowerDirectory + '/localforage/dist/localforage.js');
   }
 };
