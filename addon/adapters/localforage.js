47e3bf2273f65fd1d3512240fefaebc8c5b0aecb
iggor_kurr <kurinnoyi@gmail.com>
2015-07-03 15:48:23 +0300

    debug

--- addon/adapters/localforage.js
+++ addon/adapters/localforage.js
@@ -288,6 +288,7 @@ export default DS.Adapter.extend(Ember.Evented, {
         relationships = relationships.concat(relationshipNames.hasMany);
 
 
+        console.log('LOCALFORAGE', type, store)
         relationships.forEach(function(relationName) {
           var relationModel = type.typeForRelationship(relationName, store),
             relationEmbeddedId = record[relationName],
