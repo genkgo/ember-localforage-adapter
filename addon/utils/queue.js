933380550ce6f6e6669e4a1828fdf7ebbde5c691
Oliver Hoffmann <oliverh855@gmail.com>
2016-09-30 15:27:10 +0200

    Fixes memory leak in utils/queue.js. (#61)

--- addon/utils/queue.js
+++ addon/utils/queue.js
@@ -9,6 +9,7 @@ export default Ember.Object.extend({
 
     this.queue[queueKey] = new Ember.RSVP.Promise((resolve, reject) => {
       this.queue[queueKey - 1].then(() => {
+        this.queue.splice(queueKey - 1, 1);
         callback(resolve, reject);
       });
     });
