8525d18379027ae7711d8b596afa9188941e2bf1
Oliver <oliverh855@gmail.com>
2016-10-18 01:00:25 +0200

    Use uuid() as record id generator.

--- addon/adapters/localforage.js
+++ addon/adapters/localforage.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import DS from 'ember-data';
 import LFQueue from 'ember-localforage-adapter/utils/queue';
 import LFCache from 'ember-localforage-adapter/utils/cache';
+import {uuid} from 'ember-cli-uuid';
 
 export default DS.Adapter.extend(Ember.Evented, {
 
@@ -31,7 +32,7 @@ export default DS.Adapter.extend(Ember.Evented, {
   findRecord(store, type, id) {
     return this._getNamespaceData(type).then((namespaceData) => {
       const record = namespaceData.records[id];
-      
+
       if (!record) {
         return Ember.RSVP.reject();
       }
@@ -149,14 +150,14 @@ export default DS.Adapter.extend(Ember.Evented, {
   },
 
   generateIdForRecord() {
-    return Math.random().toString(32).slice(2).substr(0, 5);
+    return uuid();
   },
 
   // private
 
   _setNamespaceData(type, namespaceData) {
     const modelNamespace = this._modelNamespace(type);
-    
+
     return this._loadData().then((storage) => {
       if (this.caching !== 'none') {
         this.cache.set(modelNamespace, namespaceData);
