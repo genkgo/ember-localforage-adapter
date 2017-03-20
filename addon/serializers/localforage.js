579c591d016e72c7c869b2900faa2357a3e3b32a
Daniel Schlosser <d.schlosser@monks.de>
2017-03-20 13:09:46 +0100

    _shouldSerializeHasMany was prompted to public API.

--- addon/serializers/localforage.js
+++ addon/serializers/localforage.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.JSONSerializer.extend({
 
-  _shouldSerializeHasMany(snapshot, key, relationship) {
+  shouldSerializeHasMany(snapshot, key, relationship) {
     const relationshipType = snapshot.type.determineRelationshipType(relationship, this.store);
 
     if (this._mustSerialize(key)) {
