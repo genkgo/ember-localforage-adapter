b9e344aff4ad55f093e87f3155b9f21a955c9d46
Daniel Schlosser <d.schlosser@monks.de>
2017-06-21 13:21:28 +0200

    Made change compatible with prior versions.

--- addon/serializers/localforage.js
+++ addon/serializers/localforage.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
-export default DS.JSONSerializer.extend({
+var Serializer = DS.JSONSerializer.extend({
 
   shouldSerializeHasMany(snapshot, key, relationship) {
     const relationshipType = snapshot.type.determineRelationshipType(relationship, this.store);
@@ -8,10 +8,26 @@ export default DS.JSONSerializer.extend({
     if (this._mustSerialize(key)) {
       return true;
     }
-    
+
     return this._canSerialize(key) &&
       (relationshipType === 'manyToNone' ||
         relationshipType === 'manyToMany' ||
         relationshipType === 'manyToOne');
-  }  
+  }
 });
+
+// Make Serializer compatible with older versions of ember-data.
+// From ember-data 2.12.0 on there is a deprecation for
+// this._shouldSerializeHasMany. The private API has been prompted
+// to public API this.shouldSerializeHasMany. The private API will be
+// removed in ember-data 3.0.0.
+// See https://www.emberjs.com/deprecations/ember-data/v2.x/#toc_jsonserializer-shouldserializehasmany
+if( ! DS.JSONSerializer.prototype.shouldSerializeHasMany ) {
+  Serializer.reopen({
+    _shouldSerializeHasMany( snapshot, key, relationship ){
+      return this.shouldSerializeHasMany( snapshot, key, relationship );
+    }
+  });
+}
+
+export default Serializer;
