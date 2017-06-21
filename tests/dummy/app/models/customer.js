a11169ef534a4ee57dfeeed5e82042b8835f3bb8
Daniel Schlosser <d.schlosser@monks.de>
2017-06-21 16:57:49 +0200

    Fixed failing tests.

--- tests/dummy/app/models/customer.js
+++ tests/dummy/app/models/customer.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  customerNumber: DS.Model(),
+  customerNumber: DS.attr(),
   hour: DS.belongsTo('hour', { async: false }),
   addresses: DS.hasMany('address', { async: false })
 });
